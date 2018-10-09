ebb4d22728ff995a3c837b8543eeee57c2b02d8f
Marnee Dearman <marneedearman@email.arizona.edu>
2018-10-08 17:14:56 -0700

    added core and infrastructure

---
+++ src/Template.Saturn.Core/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace Template.Saturn.Core
+
+type Class1() = 
+    member this.X = "F#"
---
+++ src/Template.Saturn.Infrastructure/Library1.fs
@@ -0,0 +1,4 @@
+﻿namespace Template.Saturn.Infrastructure
+
+type Class1() = 
+    member this.X = "F#"

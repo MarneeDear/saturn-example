7e0442bd7ba10520557e127415341a3e6996d4f3
Marnee Dearman <marneedearman@email.arizona.edu>
2018-11-12 09:43:34 -0700

    added CAS auth and a route to try it

--- src/Template.Saturn.WebHost/Program.fs
+++ src/Template.Saturn.WebHost/Program.fs
@@ -1,12 +1,71 @@
 module Server
 
+open System
+open Giraffe
 open Saturn
 open Config
+//open Microsoft.AspNetCore.Authentication.OAuth
+open Microsoft.AspNetCore.Http
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Authentication.OAuth
+open System.Net.Http
+open System.Net.Http.Headers
+open Newtonsoft.Json.Linq
+open CAS
+open Microsoft.AspNetCore.Builder
+open System.Security.Claims
 
 let endpointPipe = pipeline {
     plug head
     plug requestId
 }
+
+
+let newCasOptions = new CasOptions()
+newCasOptions.CasServerUrlBase <- "https://webauth.arizona.edu/webauth"
+
+    
+
+//let login = pipeline {
+//    requires_authentication (Giraffe.Auth.challenge "CAS")
+//}
+
+//let logged_in_view = router {
+//    pipe_through login
+
+//    get "/google" (fun next ctx -> task {
+//        let name = ctx.User.Claims |> Seq.filter (fun claim -> claim.Type = ClaimTypes.Name) |> Seq.head
+//        return! json { user_name = name.Value } next ctx
+//    })
+//}
+
+//let setCustomOAuthOptions (opts:OAuthOptions) = 
+//    opts.AuthorizationEndpoint <- "https://webauth.arizona.edu/webauth"
+//    opts.CallbackPath <- new PathString("/")
+//    opts.Events <- new OAuthEvents()
+//    opts.ClientId <- "WebAuth"
+//    opts.ClientSecret <- "TEST"
+//    opts.Events.OnCreatingTicket <- 
+//        fun ctx -> 
+//            let tsk = task {
+//              let req = new HttpRequestMessage(HttpMethod.Get, ctx.Options.UserInformationEndpoint)
+//              req.Headers.Accept.Add(MediaTypeWithQualityHeaderValue("application/json"))
+//              req.Headers.Authorization <- AuthenticationHeaderValue("Bearer", ctx.AccessToken)
+//              let! (response : HttpResponseMessage) = ctx.Backchannel.SendAsync(req, HttpCompletionOption.ResponseHeadersRead, ctx.HttpContext.RequestAborted)
+//              response.EnsureSuccessStatusCode () |> ignore
+//              let! cnt = response.Content.ReadAsStringAsync()
+//              let user = JObject.Parse cnt
+//              ctx.RunClaimActions user
+//            }
+//            Task.Factory.StartNew(fun () -> tsk.Result)
+
+    //https://github.com/SaturnFramework/Saturn/blob/c32044a41a540aa82fe2d5ecac15e683716eec07/src/Saturn.Extensions.Authorization/OAuth.fs#L25
+    
+    (*
+    
+    those are always painful for me. I usually end up having to define them in pure F#, and then when setting the property on the options object wrap my function in `System.Func<_,_,_.....>`, or `System.Action<_,_,...>` with more `_` for each parameter it takes
+    eg `let app = app.Use(Func<_,_,_> applyForwardingHeaders)`
+    *)
 
 let app = application {
     pipe_through endpointPipe
@@ -19,6 +78,8 @@ let app = application {
     use_gzip
     use_config (fun _ -> {connectionString = "DataSource=database.sqlite"} ) //TODO: Set development time configuration
     use_iis
+    //use_custom_oauth "Web Auth" (fun opts -> setWebAuthOptions opts )
+    use_cas_with_options newCasOptions
 }
 
 [<EntryPoint>]
--- src/Template.Saturn.WebHost/Router.fs
+++ src/Template.Saturn.WebHost/Router.fs
@@ -3,7 +3,15 @@ module Router
 open Saturn
 open Giraffe.Core
 open Giraffe.ResponseWriters
+open Books
+open Login
+open Giraffe
+open System.Security.Claims
 
+let login = pipeline {
+    requires_authentication (Giraffe.Auth.challenge "CAS")
+    //plug print_user_details
+}
 
 let browser = pipeline {
     plug acceptHtml
@@ -16,7 +24,21 @@ let defaultView = router {
     get "/" (htmlView Index.layout)
     get "/index.html" (redirectTo false "/")
     get "/default.html" (redirectTo false "/")
+
 }
+
+type UserCredentialsResponse = { user_name : string }  
+
+
+let loginRouter = router {
+    pipe_through login
+    get "" (fun next ctx -> htmlView (Login.layout ctx) next ctx)
+    //get "" (fun next ctx -> task {
+    //    let name = ctx.User.Claims |> Seq.filter (fun claim -> claim.Type = ClaimTypes.Name) |> Seq.head
+    //    return! json { user_name = name.Value } next ctx
+    //})
+}
+
 
 let browserRouter = router {
     not_found_handler (htmlView NotFound.layout) //Use the default 404 webpage
@@ -24,7 +46,9 @@ let browserRouter = router {
 
     forward "" defaultView //Use the default view
     forward "/books" Books.Controller.resource 
+    forward "/login" loginRouter
 }
+
 
 //Other scopes may use different pipelines and error handlers
 

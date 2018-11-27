013a8c6f9e6b96fc79d11686dce3cec3683b272b
Marnee Dearman <marneedearman@email.arizona.edu>
2018-11-26 17:23:39 -0700

    Moved Authorizations into Access

---
+++ src/Template.Saturn.WebHost/Access/Authorization.fs
@@ -0,0 +1,18 @@
+﻿module Authorization
+
+open Saturn
+open Giraffe
+
+let denyAccess = pipeline {
+  set_status_code 401
+  text "Access Denied!!!!"
+
+}
+
+let onlyAllowAdmin = pipeline {
+  requires_role "admin"  denyAccess
+}
+
+let allowAccessByRoles roles = pipeline {
+  requires_role_of roles denyAccess 
+}
---
+++ src/Template.Saturn.WebHost/Access/Cas.fs
@@ -0,0 +1,65 @@
+﻿module CAS
+
+open Saturn
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Authentication.Cookies
+open Microsoft.AspNetCore.Authentication
+open AspNetCore.Security.CAS
+open System.Security.Claims
+open System.Threading.Tasks
+open COM.Core
+open COM.Infrastructure
+open COM.Core.Models
+
+
+let private addCookie state (c : AuthenticationBuilder) = if not state.CookiesAlreadyAdded then c.AddCookie() |> ignore
+
+type ApplicationBuilder with
+    //Enables CAS authentication
+    //Uses https://github.com/IUCrimson/AspNet.Security.CAS
+    [<CustomOperation("use_cas")>]
+    member __.UseCasAuthentication(state: ApplicationState, casServerUrlBase, edsServerUrl, edsUserName, edsPassword) =
+      let middleware (app : IApplicationBuilder) =
+        app.UseAuthentication() 
+        
+      let edsService =
+        new DirectoryServices.EdsClient(edsServerUrl, edsUserName,
+            edsPassword) :> Interfaces.IEdsClient
+
+      let getClaims (attributes: UserAttribute list) =
+        let identity = new ClaimsIdentity()
+        for attr in attributes do 
+          for value in attr.Values do
+            if attr.Name = "isMemberOf" then
+              identity.AddClaim(new Claim(ClaimTypes.Role, value))
+            else
+              identity.AddClaim(new Claim(attr.Name, value))
+            identity.AddClaim(new Claim(ClaimTypes.Role, "admin"))
+        identity
+
+      let cookieEvents = new CookieAuthenticationEvents()
+      cookieEvents.OnSigningIn <- (fun ctx -> 
+            let userInfo = edsService.GetUserInfo(ctx.Principal.Identity.Name)
+            ctx.Principal.AddIdentity(getClaims(userInfo.Attributes))
+            Task.CompletedTask)
+
+      let service (s : IServiceCollection) =
+        let c = s.AddAuthentication(fun cfg ->
+          cfg.DefaultScheme <- CookieAuthenticationDefaults.AuthenticationScheme
+          cfg.DefaultChallengeScheme <- "CAS"
+          )
+        c.AddCookie(fun o -> o.Events <- cookieEvents) |> ignore
+        //addCookie state c
+        c.AddCAS(fun o -> 
+            o.CasServerUrlBase <- casServerUrlBase
+            o.SignInScheme <- CookieAuthenticationDefaults.AuthenticationScheme
+            )
+        |> ignore
+        s
+
+      { state with
+          ServicesConfig = service::state.ServicesConfig
+          AppConfigs = middleware::state.AppConfigs
+          CookiesAlreadyAdded = true
+      }

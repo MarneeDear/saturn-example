381699baccc97c123657f16034fd49c457d8eed2
Marnee Dearman <marneedearman@email.arizona.edu>
2018-12-28 15:33:14 -0700

    Changed build to use the fake runner and added the fable dependencies

--- build-dotnet.fsx
+++ build-dotnet.fsx
@@ -1,42 +1,113 @@
-#r @"packages/build/FAKE/tools/FakeLib.dll"
+//#r @"packages/build/FAKE/tools/FakeLib.dll"
+#r "paket: groupref build //"
+#load "./.fake/build-dotnet.fsx/intellisense.fsx"
+
+#if !FAKE
+#r "netstandard"
+#r "Facades/netstandard" // https://github.com/ionide/ionide-vscode-fsharp/issues/839#issuecomment-396296095
+#endif
+
 
 open System
-open Fake
+//open Fake
 open Fake.DotNet
 open Fake.Core
 open Fake.IO
 
 let appPath = "./src/Template.Saturn.WebHost/" |> Fake.IO.Path.getFullName
 let infrastructureTestsPath = "./src/Template.Saturn.Infrastructure.Tests" |> Fake.IO.Path.getFullName
+//let serverPath = Path.getFullName "./src/Server"
+//let clientPath = Path.getFullName "./src/Client"
+//let deployDir = Path.getFullName "./deploy"
 
-Core.Target.create "InstallDotNetCore" (fun _ ->
+let platformTool tool winTool =
+    let tool = if Environment.isUnix then tool else winTool
+    match ProcessUtils.tryFindFileOnPath tool with
+    | Some t -> t
+    | _ ->
+        let errorMsg =
+            tool + " was not found in path. " +
+            "Please install it and make sure it's available from your path. " +
+            "See https://safe-stack.github.io/docs/quickstart/#install-pre-requisites for more info"
+        failwith errorMsg
+
+let runDotNet cmd workingDir =
+    let result =
+        DotNet.exec (DotNet.Options.withWorkingDirectory workingDir) cmd ""
+    if result.ExitCode <> 0 then failwithf "'dotnet %s' failed in %s" cmd workingDir
+
+let runTool cmd args workingDir =
+    let arguments = args |> String.split ' ' |> Arguments.OfArgs
+    Command.RawCommand (cmd, arguments)
+    |> CreateProcess.fromCommand
+    |> CreateProcess.withWorkingDirectory workingDir
+    |> CreateProcess.ensureExitCode
+    |> Proc.run
+    |> ignore
+
+let openBrowser url =
+    //https://github.com/dotnet/corefx/issues/10361
+    Command.ShellCommand url
+    |> CreateProcess.fromCommand
+    |> CreateProcess.ensureExitCodeWithMessage "opening browser failed"
+    |> Proc.run
+    |> ignore
+
+let nodeTool = platformTool "node" "node.exe"
+let yarnTool = platformTool "yarn" "yarn.cmd"
+
+Target.create "InstallDotNetCore" (fun _ ->
     DotNet.install (fun p -> {p with Version = DotNet.CliVersion.GlobalJson }) |> ignore
 )
 
-Core.Target.create "Restore" (fun _ ->
+Target.create "InstallClient" (fun _ ->
+    printfn "Node version:"
+    runTool nodeTool "--version" __SOURCE_DIRECTORY__
+    printfn "Yarn version:"
+    runTool yarnTool "--version" __SOURCE_DIRECTORY__
+    runTool yarnTool "install --frozen-lockfile" __SOURCE_DIRECTORY__
+    //runDotNet "restore" clientPath
+)
+
+Target.create "Restore" (fun _ ->
     DotNet.restore (fun p -> p) appPath |> ignore
 )
 
 open Fake.IO.FileSystemOperators
 
-Core.Target.create "RenameConfig" (fun _ ->
+Target.create "RenameConfig" (fun _ ->
     if not (File.exists(appPath @@ "config.yaml"))
         then Fake.IO.Shell.rename (appPath @@ "config.yaml") (appPath @@ "config_design.yaml") |> ignore
 )
 
-Core.Target.create "Build"  (fun _ ->
+Target.create "Build"  (fun _ ->
     //DotNetCli.Build(fun p -> {p with WorkingDir = appPath})
-    DotNet.build (fun p -> p) "" |> ignore
+    //DotNet.build (fun p -> p) "" |> ignore
+    runDotNet "build" appPath
+    //runTool yarnTool "webpack-cli --config src/Client/webpack.config.js -p" __SOURCE_DIRECTORY__
 )
 
-Core.Target.create "Run" (fun _ -> 
+Target.create "Run" (fun _ -> 
   let server = async {
-    DotNet.exec (fun p -> {p with WorkingDirectory = appPath}) "watch run" "" |> ignore
+    runDotNet "watch run" appPath |> ignore
     }
+
+  let client = async {
+        runTool yarnTool "webpack-dev-server --config src/Client/webpack.config.js" __SOURCE_DIRECTORY__
+    }
+
   let browser = async {
     Threading.Thread.Sleep 5000
-    Diagnostics.Process.Start "http://saturn.local:8085" |> ignore
+    openBrowser "http://saturn.local:8085" |> ignore
   }
+  let vsCodeSession = Environment.hasEnvironVar "vsCodeSession"
+  let safeClientOnly = Environment.hasEnvironVar "safeClientOnly"
+
+  let tasks =
+    [ if not safeClientOnly then yield server
+      yield client
+      if not vsCodeSession then yield browser ]
+
 
   //TODO consider running these in order. wait for the server to start up all the way first
   [ server; browser]
@@ -45,15 +116,15 @@ Core.Target.create "Run" (fun _ ->
   |> ignore
 )
 
-Core.Target.create "Test" (fun _ -> 
+Target.create "Test" (fun _ -> 
     DotNet.test (fun p -> p) infrastructureTestsPath
 )
 
-Core.Target.create "Clean" (fun _ ->
+Target.create "Clean" (fun _ ->
     () //TODO cleanup the build folder
 )
 
-Core.Target.create "Publish" (fun _ ->
+Target.create "Publish" (fun _ ->
     DotNet.publish (fun p -> { p with OutputPath = Some "../../published"} ) appPath
 )
 
@@ -83,5 +154,5 @@ open Fake.Core.TargetOperators
   ==> "Publish"
 
 
-Core.Target.runOrDefault "Test"
+Target.runOrDefaultWithArguments "Test"
 
